e13743c8933248d1220dd2d75bf44ef65293742e
CraciDenisStefan <crdenis13@gmail.com>
2023-06-09 15:35:52 +0200

    [FEAT]Creato task in html preso dal js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,31 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      count: 0
+        tasks : [ {
+            text:'Fare colazione',
+            done: false 
+        },
+        {
+            text:'Uscire a corrrere',
+            done: false 
+        },
+        {
+            text:'Doccia',
+            done: false 
+        },
+        {
+            text:'Studiare',
+            done: false 
+        },
+        {
+            text:'Leggere',
+            done: false 
+        },
+        {
+            text:'Gioca',
+            done: false 
+        },
+    ]
     }
   }
 }).mount('#app')
